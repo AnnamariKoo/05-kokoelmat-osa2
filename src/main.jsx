9c6f66322f77e0c79d974f8ef050bc5de667544a
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-10-09 09:40:56 +0300

    Datan hakeminen main.jsx poistettu ja App-komponenttia päivitetty (kohta: effect-hookit)

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,38 @@
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-const result = notes.map((note) => note.content);
-console.log(result);
+// import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App notes={notes} />
-);
+// axios.get("http://localhost:3001/notes").then((response) => {
+//   const notes = response.data;
+//   ReactDOM.createRoot(document.getElementById("root")).render(
+//     <App notes={notes} />
+//   );
+// });
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
+
+// const result = notes.map((note) => note.content);
+// console.log(result);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <App notes={notes} />
+// );
