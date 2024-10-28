f38ad48767f9de94242c696607fb12fc8a6766bd
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-10-28 09:10:12 +0200

    Css-muotoilut päivitetty tehdyksi ohjeistuksen mukaan omilla tyyleillä

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 
-import "./App.css";
+// import "./index.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
